fbfcc5e427d4975ec8aa2f6226878d57a3778103
LeoSirius <is.li.xiaoyu@qq.com>
2019-08-23 10:02:18 +0800

    update

--- tmp.c
+++ tmp.c
@@ -1,7 +1,32 @@
-#include<stdio.h>
+/*
+** 计算一个数的平方根
+*/
 
-int main()
+#include<stdio.h>
+#include<stdlib.h>
+
+int main ()
 {
-    printf("??)\n");
-    return 0;
+    float new_guess;
+    float last_guess;
+    float number;
+
+    printf("Enter a number: ");
+    scanf("%f", &number);
+
+    if (number < 0) {
+        printf("Cannot compute the square root of a negative number!\n");
+        return EXIT_FAILURE;
+    }
+
+    new_guess = 1;
+    do {
+        last_guess = new_guess;
+        new_guess = (last_guess + number / last_guess) / 2;
+        printf("%.15e\n", new_guess);    /* 浮点数、e-记数法 */
+    } while (new_guess != last_guess);
+
+    printf("Square root of %g is %g\n", number, new_guess);     /* %g根据数值不同自动选择%f或%e */
+
+    return EXIT_SUCCESS;
 }
