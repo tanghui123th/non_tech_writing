2658de6f691d3283929a34821bd454492a882b17
LeoSirius <is.li.xiaoyu@qq.com>
2019-08-24 10:54:33 +0800

    update

--- tmp.c
+++ tmp.c
@@ -1,32 +1,36 @@
-/*
-** 计算一个数的平方根
-*/
-
 #include<stdio.h>
-#include<stdlib.h>
 
-int main ()
+unsigned int reverse_bits (unsigned int value)
 {
-    float new_guess;
-    float last_guess;
-    float number;
+    unsigned int answer;
+    unsigned int i;
 
-    printf("Enter a number: ");
-    scanf("%f", &number);
+    answer = 0;
 
-    if (number < 0) {
-        printf("Cannot compute the square root of a negative number!\n");
-        return EXIT_FAILURE;
+    /*
+    ** 这样写回循环int的位数次，保证了与特定机器字长无关。value向左走，answer向右走，两者是mirror。
+    */
+    for (i = 1; i != 0; i <<= 1) {
+        answer <<= 1;
+        if (value & 1) answer |= 1;    /* 如果value最低位是1，则把answer的最低位也置1 */
+        value >>= 1;
     }
+    return answer;
+}
 
-    new_guess = 1;
-    do {
-        last_guess = new_guess;
-        new_guess = (last_guess + number / last_guess) / 2;
-        printf("%.15e\n", new_guess);    /* 浮点数、e-记数法 */
-    } while (new_guess != last_guess);
+int test()
+{
+    unsigned int value;
+    unsigned int answer;
 
-    printf("Square root of %g is %g\n", number, new_guess);     /* %g根据数值不同自动选择%f或%e */
+    value = 25;
+    answer = 2550136832;
+    if (reverse_bits(value) == 2550136832) return 1;
+    else return 0;
+}
 
-    return EXIT_SUCCESS;
+int main()
+{
+    if (test()) printf("Success.\n");
+    else printf("Failed.\n");
 }
