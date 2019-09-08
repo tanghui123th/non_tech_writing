061799edc4c02d6114f0dee7252bdf8bbc21a284
LeoSirius <is.li.xiaoyu@qq.com>
2019-09-09 00:07:56 +0800

    update

--- tmp.c
+++ tmp.c
@@ -1,18 +1,12 @@
 #include<stdio.h>
-#include<stdlib.h>
-
-void
-mystery (int n)
-{
-    n += 5;
-    n /= 10;
-    printf("%s\n", "**********" + 10 - n);
-}
 
 int
-main (int argc, char **argv)
+main ()
 {
-    printf("%c\n", *("xyz" + 1));   // "xyz" + 1指向y，结果输出y
-    printf("%c\n", "xyz"[2]);       // "xyz"[2]指向z，结果输出z
-    return EXIT_SUCCESS;
+    printf("__FILE__ = %s\n", __FILE__);
+    printf("__LINE__ = %d\n", __LINE__);
+    printf("__DATE__ = %s\n", __DATE__);
+    printf("__TIME__ = %s\n", __TIME__);
+    printf("__STDC__ = %d\n", __STDC__);
+    return 0;
 }
