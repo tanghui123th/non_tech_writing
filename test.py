776ade4ad2d2ca38a7a700b2bf0638217526ef4f
LeoSirius <is.li.xiaoyu@qq.com>
2019-10-29 17:50:35 +0800

    update

--- test.py
+++ test.py
@@ -1,8 +1,47 @@
-import json
+from random import choice
+
+import matplotlib.pyplot as plt
 
 
-filename = 'person_info.json'
-with open(filename) as f_ojb:
-    info = json.load(f_ojb)
+class RandomWalk:
 
-print(info)   # 会输出  {'name': 'leo sirius', 'age': 24, 'gender': None}
+    def __init__(self, num_points=5000):
+        self.num_points = num_points
+
+        # 从(0, 0)开始
+        self.x_values = [0]
+        self.y_values = [0]
+
+    def fill_walk(self):
+        while len(self.x_values) < self.num_points:
+            x_direction = choice([1, -1])
+            x_distance = choice([0, 1, 2, 3, 4])
+            x_step = x_direction * x_distance
+
+            y_direction = choice([1, -1])
+            y_distance = choice([0, 1, 2, 3, 4])
+            y_step = y_direction * y_distance
+
+            # 拒绝原地踏步
+            if x_step == 0 and y_step == 0:
+                continue
+
+            # 计算下一个点的x和y值
+            next_x = self.x_values[-1] + x_step
+            next_y = self.y_values[-1] + y_step
+
+            self.x_values.append(next_x)
+            self.y_values.append(next_y)
+
+
+while True:
+    rw = RandomWalk()
+    rw.fill_walk()
+
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, s=15)
+    plt.show()
+
+    keep_running = input('Make another walk? (y/n): ')
+    if keep_running == 'n':
+        break
